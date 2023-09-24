f699ca694b7c1d8c47e76cefa60e74f367a82b97
Sheetal02525 <sheetal.20gcebcs040@galgotiacollege.edu>
2023-09-24 12:50:05 +0530

    biparite

---
+++ Striver Sheet/Biparite Graph/biparite.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int n=graph.length;
+        int[] colors=new int[n];
+        
+        for(int i=0;i<n;i++){
+            if(colors[i]==0  &&  !dfs(graph,colors,1,i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean dfs(int[][] graph,int[] colors,int color,int node){
+        if(colors[node]!=0){
+            return colors[node]==color;
+        }
+        colors[node]=color;
+        for(int next:graph[node]){
+            if(!dfs(graph,colors,-color,next)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
